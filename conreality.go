382fc698c04c6e71525d3bf81c43473d3e2f5450
Arto Bendiken <arto@conreality.org>
2017-06-21 00:48:50 +0200

    Improved the package description.

--- conreality.go
+++ conreality.go
@@ -1,6 +1,6 @@
 /* This is free and unencumbered software released into the public domain. */
 
-// Conreality Software Development Kit (SDK) for Go.
+// Package conreality provides the Conreality Software Development Kit (SDK) for Go.
 package conreality
 
 import (
