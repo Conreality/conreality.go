339af05b19b2d04afdd403b78da262e12549d136
Arto Bendiken <arto@conreality.org>
2017-06-21 00:26:17 +0200

    Declared the initial set of types.

--- conreality.go
+++ conreality.go
@@ -3,4 +3,47 @@
 // Conreality Software Development Kit (SDK) for Go.
 package conreality
 
+import (
+	"database/sql"
+	"github.com/satori/go.uuid"
+)
+
 const Version = "0.0.0"
+
+type Asset struct {
+	uuid uuid.UUID
+}
+
+type Binary struct {
+	id uint64
+}
+
+type Camera struct {
+	uuid uuid.UUID
+}
+
+type Client struct {
+	db *sql.DB
+}
+
+type Event struct {
+	id uint64
+}
+
+type Message struct {
+	id uint64
+}
+
+type Object struct {
+	uuid uuid.UUID
+}
+
+type Player struct {
+	uuid uuid.UUID
+}
+
+type Session struct{}
+
+type Theater struct {
+	uuid uuid.UUID
+}
