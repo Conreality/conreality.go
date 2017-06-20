3d57d5ca34a1bd4b539de5935a8f2e741d7c0936
Arto Bendiken <arto@conreality.org>
2017-06-21 00:53:44 +0200

    Defined a test suite for the SDK.

---
+++ conreality_test.go
@@ -0,0 +1,12 @@
+/* This is free and unencumbered software released into the public domain. */
+
+package conreality
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
