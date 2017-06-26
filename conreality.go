da0b63dce80d65fa2f9c7defe5301fd160b94070
Arto Bendiken <arto@conreality.org>
2017-06-27 01:13:36 +0200

    Implemented Connect() and Client.Disconnect().

--- conreality.go
+++ conreality.go
@@ -5,13 +5,14 @@ package conreality
 
 import (
 	"database/sql"
+	_ "github.com/lib/pq"
 	"github.com/satori/go.uuid"
 )
 
 const Version = "0.0.0"
 
 type Asset struct {
-	uuid uuid.UUID
+	Object
 }
 
 type Binary struct {
@@ -19,7 +20,7 @@ type Binary struct {
 }
 
 type Camera struct {
-	uuid uuid.UUID
+	Object
 }
 
 type Client struct {
@@ -39,11 +40,38 @@ type Object struct {
 }
 
 type Player struct {
-	uuid uuid.UUID
+	Object
 }
 
 type Session struct{}
 
 type Theater struct {
 	uuid uuid.UUID
+}
+
+// Connect attempts to connect to a local master server.
+//
+// The returned handle is safe for concurrent use by multiple goroutines and
+// maintains its own internal pool of idle connections. Thus, the Connect
+// function should be called just once. It is rarely necessary to close a
+// handle.
+func Connect(gameName string) (*Client, error) {
+	var db, err = sql.Open("postgres", "sslmode=disable dbname="+gameName)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return &Client{db: db}, nil
+}
+
+// Disconnect closes the connection to the master server.
+func (client *Client) Disconnect() error {
+	var err = client.db.Close()
+	if err != nil {
+		return err
+	}
+	client.db = nil
+	return nil
 }
