563966f5093d0909aa5e1d8903c8dd68a41fbb5f
Arto Bendiken <arto@conreality.org>
2019-01-17 20:48:26 +0200

    Implemented sdk.LookupEntityByName().

---
+++ sdk/lookup_entity_by_name.go
@@ -0,0 +1,20 @@
+/* This is free and unencumbered software released into the public domain. */
+
+package sdk
+
+import (
+	"context"
+
+	rpc "github.com/conreality/conreality.go/rpc"
+	"github.com/pkg/errors"
+)
+
+// LookupEntityByName TODO...
+func (session *Session) LookupEntityByName(ctx context.Context, entityName string) (*Entity, error) {
+	request := &rpc.String{Value: entityName}
+	response, err := session.client.session.LookupEntityByName(ctx, request)
+	if err != nil {
+		return nil, errors.Wrap(err, "LookupEntityByName failed")
+	}
+	return &Entity{ID: response.Value}, nil
+}
